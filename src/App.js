266a615661c560e17419e4d1a45912ca66442d95
chugumba <chugumba@rambler.ru>
2024-03-29 21:10:36 +0300

    Начальная разметка
    ISU-2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header style={{ backgroundColor: 'lightblue', color: 'white', padding: '20px' }}>
+      <nav>
+        <ul style={{ display: 'flex', listStyle: 'none', margin: 0, padding: 0 }}>
+          <li style={{ marginRight: '20px' }}><a href="#">О нас</a></li>
+          <li style={{ marginRight: '20px' }}><a href="#">Клиентам</a></li>
+          <li style={{ marginRight: '20px' }}><a href="#">Регулятивная информация</a></li>
+          <li style={{ marginRight: '20px' }}><a href="#">Контакты</a></li>
+          <li style={{ marginRight: '20px' }}><a href="#">Вакансии</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      
+    </main>
+  </div>
   );
 }
 
