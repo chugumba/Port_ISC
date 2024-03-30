d33cd9fdb78b082ed4f441968bf4e933132179c0
chugumba <chugumba@rambler.ru>
2024-03-30 21:04:53 +0300

    Промежуточное сохранение изменений
    ISU-2
    
    Добавлен логотип, изменены стили

--- src/components/mainHeader.jsx
+++ src/components/mainHeader.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import '../styles/header.css';
+import logoImage from '../img/logo.jpg'
 
 const MainHeader = function () {
     return (
 
-    <header className="main-header">
-      <nav className='main-nav'>
-        <ul className='main-nav-menu'>
-          <li className='main-header-item'><a href="#">О нас</a></li>
-          <li className='main-header-item'><a href="#">Клиентам</a></li>
-          <li className='main-header-item'><a href="#">Регулятивная информация</a></li>
-          <li className='main-header-item'><a href="#">Контакты</a></li>
-          <li className='main-header-item'><a href="#">Вакансии</a></li>
-        </ul>
-      </nav>
+    <header className='main-header'>
+        <div className='header-container'>
+                <div className='header-left'>
+                    <a href="#" className="header-logo-action">
+                    <img src={logoImage} alt='Logo' className="header-logo"/>
+                    <div className='header-text'>
+                        <span>Речной Порт</span>
+                        <span>"Волга"</span>
+                    </div>
+                    </a>
+                </div>
+                <nav className='main-nav'>
+                    <ul className='main-nav-menu'>
+                        <li className='main-header-item'><a href="#">О нас</a></li>
+                        <li className='main-header-item'><a href="#">Клиентам</a></li>
+                        <li className='main-header-item'><a href="#">Регулятивная информация</a></li>
+                        <li className='main-header-item'><a href="#">Контакты</a></li>
+                        <li className='main-header-item'><a href="#">Вакансии</a></li>
+                    </ul>
+                </nav>
+        </div>
     </header>
 
     );
