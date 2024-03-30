07c6ba274ecdd2965593c87e84fd38e66e5880ae
chugumba <chugumba@rambler.ru>
2024-03-30 13:53:16 +0300

    Модифицирован header
    ISU-2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, {useState} from 'react'
-
+import './styles/main.css';
 function App() {
 
   return (
-    <div className="App">
-    <header style={{ backgroundColor: 'lightblue', color: 'white', padding: '20px' }}>
-      <nav>
-        <ul style={{ display: 'flex', listStyle: 'none', margin: 0, padding: 0 }}>
-          <li style={{ marginRight: '20px' }}><a href="#">О нас</a></li>
-          <li style={{ marginRight: '20px' }}><a href="#">Клиентам</a></li>
-          <li style={{ marginRight: '20px' }}><a href="#">Регулятивная информация</a></li>
-          <li style={{ marginRight: '20px' }}><a href="#">Контакты</a></li>
-          <li style={{ marginRight: '20px' }}><a href="#">Вакансии</a></li>
+<div className="App">
+    <header className="main-header" style={{ backgroundColor: 'lightblue', color: 'white', padding: '20px' }}>
+      <nav className='main-nav'>
+        <ul className='main-nav-menu'>
+          <li className='main-header-item'><a href="#">О нас</a></li>
+          <li className='main-header-item'><a href="#">Клиентам</a></li>
+          <li className='main-header-item'><a href="#">Регулятивная информация</a></li>
+          <li className='main-header-item'><a href="#">Контакты</a></li>
+          <li className='main-header-item'><a href="#">Вакансии</a></li>
         </ul>
       </nav>
     </header>
