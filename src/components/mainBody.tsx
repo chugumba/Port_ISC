58f89d43473f9372abb208d4d2fe26be86ab879c
chugumba <chugumba@rambler.ru>
2024-04-04 18:15:00 +0300

    Переход на Vite
    ISU-4
    
    Был осуществлён переход на Vite вместо CRA для корректной работы компонентов

--- src/components/mainBody.tsx
+++ src/components/mainBody.tsx
@@ -1,22 +1,30 @@
 import React, { useRef } from 'react';
 import Autoplay from 'embla-carousel-autoplay';
 import { Carousel } from '@mantine/carousel';
+import '../styles/main/main.css';
 
 function MainBody() {
   const autoplay = useRef(Autoplay({ delay: 2000 }));
   return (
-    <Carousel
-      withIndicators
-      height={200}
-      plugins={[autoplay.current]}
-      onMouseEnter={autoplay.current.stop}
-      onMouseLeave={autoplay.current.reset}
-    >
-      <Carousel.Slide>1</Carousel.Slide>
-      <Carousel.Slide>2</Carousel.Slide>
-      <Carousel.Slide>3</Carousel.Slide>
-      {/* ...other slides */}
-    </Carousel>
+
+    <main>
+      <div className='main-main-container'>
+        <div className='main-carousele-container'>
+          <Carousel className='carousele-main'
+            withIndicators
+            height={200}
+            plugins={[autoplay.current]}
+            onMouseEnter={autoplay.current.stop}
+            onMouseLeave={autoplay.current.reset}
+          >
+            <Carousel.Slide>1</Carousel.Slide>
+            <Carousel.Slide>2</Carousel.Slide>
+            <Carousel.Slide>3</Carousel.Slide>
+            {/* ...other slides */}
+          </Carousel>
+      </div>
+      </div>
+    </main>
   );
 }
 
